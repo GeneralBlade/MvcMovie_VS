95f32af80e0d61c01df9d5ec06822853371e1cc9
Andrey <anitczn@gmail.com>
2018-04-04 11:26:52 +0400

    Добавил комментарий в мови-контроллер

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-ZА-Я]+[a-zа-яA-ZА-Я ""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[0-9]+[\s+]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
+    }
+}
